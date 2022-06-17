c853fa83679edcfe7779e7e0d45c2d23b5183c05
vadim <vadimrk@email.ua>
2022-06-17 22:07:16 +0300

    refact build case only method

--- src/main/java/com/gmail/wformat/util/BuildCases.java
+++ src/main/java/com/gmail/wformat/util/BuildCases.java
@@ -28,34 +28,38 @@ public class BuildCases {
 //    public static final String EXCLUDE_DESCRIPTIONS = "(\\(%1$s[,\\)])|(, %1$s\\))";
 
 
-    public static Case incl = new Case("include", "incl", PRE_INCLUDE, INCL_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
-    public static Case attr = new Case("attribute", "attr", PRE_ATTRIBUTES, ATTRIBUTES_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
-    public static Case dataArr = new Case("data_array", "d_arr", PRE_DATA_ARR, DATA_ARR_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
-    public static Case data = new Case("data", "data", PRE_DATA, DATA_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
-    public static Case fullOptions = new Case("fullOptions", "full", PRE_FULL_OPTIONS, FULL_OPTIONS_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
+    public static Case incl;// = new Case("include", "incl", PRE_INCLUDE, INCL_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
+    public static Case attr;// = new Case("attribute", "attr", PRE_ATTRIBUTES, ATTRIBUTES_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
+    public static Case dataArr;// = new Case("data_array", "d_arr", PRE_DATA_ARR, DATA_ARR_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
+    public static Case data;// = new Case("data", "data", PRE_DATA, DATA_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
+    public static Case fullOptions;// = new Case("fullOptions", "full", PRE_FULL_OPTIONS, FULL_OPTIONS_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
     public static Case manualOptions;// = new Case("manualOptions", "manual", PRE_FULL_OPTIONS, null /*, EXCLUDE_DESCRIPTIONS*/);
 
 
     private static Set<Case> cases = new HashSet<>();
 
     public static void addCaseInclude() {
+        incl = new Case("include", "incl", PRE_INCLUDE, INCL_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
         cases.add(incl);
     }
 
     public static void addCaseAttribute() {
+        attr = new Case("attribute", "attr", PRE_ATTRIBUTES, ATTRIBUTES_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
         cases.add(attr);
     }
 
     public static void addCaseDataArr() {
+        dataArr = new Case("data_array", "d_arr", PRE_DATA_ARR, DATA_ARR_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
         cases.add(dataArr);
     }
 
     public static void addCaseData() {
+        data = new Case("data", "data", PRE_DATA, DATA_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
         cases.add(data);
     }
 
     public static void addFullOptions() {
-
+        fullOptions = new Case("fullOptions", "full", PRE_FULL_OPTIONS, FULL_OPTIONS_QUOTES/*, EXCLUDE_DESCRIPTIONS*/);
         cases.add(fullOptions);
     }
 
